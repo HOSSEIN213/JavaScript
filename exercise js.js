50aa32ffc33ae900a8d177c36dfc2690e91f9454
HOSSEIN213 <hossin.ravan82@gmail.com>
2025-03-18 16:48:57 +0330

    practice part 2

--- exercise js.js
+++ exercise js.js
@@ -1,8 +1,22 @@
-let age = "25";
-console.log(age);
-console.log(typeof age);
+// let age = "88";
+// console.log(age);
+// console.log(typeof age);
 
-let cestedaAge1 =Number(age);
-console.log(cestedaAge1);
-console.log(typeof cestedaAge1);
+// let cestedaAge1 =String(age);
+// console.log(cestedaAge1);
+// console.log(typeof cestedaAge1);
 
+// let castedaAge2 = +age;
+// console.log(castedaAge2);
+// console.log(typeof castedaAge2);
+
+
+
+
+let agenew2 ="55"
+console.log(agenew2)
+console.log(typeof agenew2)
+
+let  agenwe5= +agenew2;
+console.log(agenwe5)
+console.log(typeof agenwe5)
