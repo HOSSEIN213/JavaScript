511a3631e1e26c6fa352af8a1bf078b63a704db6
HOSSEIN213 <hossin.ravan82@gmail.com>
2025-03-26 21:03:17 +0330

    new

--- exercise js.js
+++ exercise js.js
@@ -11,7 +11,7 @@
 // console.log(typeof castedaAge2);
 
 
-// pracitce p.2.3
+pracitce p.2.3
 
 // let agenew2 ="55"
 // console.log(agenew2)
@@ -32,11 +32,11 @@
 // console.log(typeof newcommit5)m
 // let commitnwe = prompt("what you are name? ")
 // console.log (commitnwe);
-// console.log (typeof commitnwe );
-// let hinew2 = +prompt ( "how are you ?");
-// console.log( hinew2 );
-// let hinew5 = "hi"
-// alert ( hinew5)
+console.log (typeof commitnwe );
+let hinew2 = +prompt ( "how are you ?");
+console.log( hinew2 );
+let hinew5 = "hi"
+alert ( hinew5)
 let newhi3 = confirm (" are you taecher ")
 let commit = "hi";
 console.log (newhi3);
@@ -44,3 +44,22 @@ console.log (newhi3);
 
  alert (newhi3) ;
 console.log (hi);
+console.log (typeof commitnwe );
+let hinew2 = +prompt ( "how are you ?");
+console.log( hinew2 );
+let hinew5 = "hi"
+alert ( hinew5) 
+
+console.log (typeof commitnwe );
+let hinew2 = +prompt ( "how are you ?");
+console.log( hinew2 );
+let hinew5 = "hi"
+alert ( hinew5)console.log (typeof commitnwe );
+let hinew2 = +prompt ( "how are you ?");
+console.log( hinew2 );
+let hinew5 = "hi"
+alert ( hinew5)console.log (typeof commitnwe );
+let hinew2 = +prompt ( "how are you ?");
+console.log( hinew2 );
+let hinew5 = "hi"
+alert ( hinew5)
